33df4d7027c14e752e21c517d42c81776c30e6cf
Allen Jiang <alljiang@hotmail.com>
2021-03-03 20:06:04 -0600

    Update TB_PulseGenerator.v

--- Lab 3/Lab3_VivadoProject/Lab3_VivadoProject.srcs/sim_1/new/TB_PulseGenerator.v
+++ Lab 3/Lab3_VivadoProject/Lab3_VivadoProject.srcs/sim_1/new/TB_PulseGenerator.v
@@ -3,16 +3,21 @@
 module TB_PulseGenerator();
 
     reg clk = 0;
+    reg start = 0;
     reg[1:0] mode;
     wire pulse;
  
     always #1000 clk = ~clk;
     
-    PulseGenerator gen(clk, mode, pulse);
+    PulseGenerator gen(clk, start, mode, pulse);
     
     initial begin
-    
-        mode = 2'b11;
+        mode = 2'b10;
+        start = 0;
+        #1000
+        start = 1;
+        #300000000
+        start = 0;
     
     end
 
