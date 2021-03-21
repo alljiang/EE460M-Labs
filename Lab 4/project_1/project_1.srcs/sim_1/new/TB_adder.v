787510967396ee57271379d5ff293f60059c18bd
xchenyi <47702865+xchenyi@users.noreply.github.com>
2021-03-21 18:28:44 -0500

    adder work and tested

---
+++ Lab 4/project_1/project_1.srcs/sim_1/new/TB_adder.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/21/2021 06:10:38 PM
+// Design Name: 
+// Module Name: TB_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TB_adder();
+
+    reg clk = 0;
+    reg btnU = 0;
+    reg btnD = 0; 
+    reg btnR = 0;
+    reg btnL = 0;
+    reg [14:0] regVal = 0;
+    wire[14:0] toDecr;
+
+always #1 clk = ~clk;
+
+Adder add(clk, btnU, btnD, btnR, btnL, regVal, toDecr);
+
+initial begin
+    regVal = 9000;
+    #10
+    btnU = 1;
+    //regVal should = 9010
+    regVal = toDecr;
+    #10
+    btnU = 0;
+    btnL = 1;
+    regVal = toDecr;
+    //regval should = 9190
+    #10
+    btnL = 0;
+    btnR = 1;
+    regVal = toDecr;
+    //regVal should = 9390
+    #10
+    btnR = 0;
+    btnD = 1;
+    regVal = toDecr;
+    //regVal should = 9390+550 = 9940
+    #10
+    btnD = 0;
+    btnR = 1;
+    regVal = toDecr;
+    //regVal should overflow and stay 9999
+    #10
+    btnR = 0;
+    btnU = 1;
+    regVal = toDecr;
+    //regval should stay 9999
+end
+
+
+endmodule
