dbe8272317fcf05fa69d65c133846736e4f54cfc
Allen Jiang <alljiang@hotmail.com>
2021-03-20 20:33:18 -0500

    Update SpeedwalkTime.v

--- Lab 3/Lab3_VivadoProject/Lab3_VivadoProject.srcs/sources_1/new/SpeedwalkTime.v
+++ Lab 3/Lab3_VivadoProject/Lab3_VivadoProject.srcs/sources_1/new/SpeedwalkTime.v
@@ -10,36 +10,33 @@ module SpeedwalkTime(
     );
     
    integer pulseCount = 0;
+   integer lastCount = 0;
    integer secondsCount = 0;
    reg[15:0] fastSecondsCount = 0;
    time tickCount = 0;
    
    assign seconds[15:0] = fastSecondsCount[15:0];
-    
-   reg lastPulse = 0;
+   
+   always @(posedge pulse) begin
+        if(reset) pulseCount <= 0;
+        else pulseCount = pulseCount + 1;
+   end
     
    always @(posedge clk) begin
    
        if(reset) begin
-           secondsCount = 0;
-           fastSecondsCount = 0;
-           tickCount = 0;
-           pulseCount = 0;
-           lastPulse = 0;
+           secondsCount <= 0;
+           fastSecondsCount <= 0;
+           tickCount <= 0;
+           lastCount <= 0;
        end
        else begin
-       
-           if(!lastPulse && pulse) begin
-                pulseCount = pulseCount + 1;
-           end
-           lastPulse = pulse;
-           
            if(secondsCount <= 9) begin
-               tickCount = tickCount + 1;
-               if(tickCount >= secondsCount * `TPS) begin
-                   secondsCount = secondsCount + 1;
-                   if(pulseCount > 32) fastSecondsCount = fastSecondsCount + 1;
-                   pulseCount = 0;
+               tickCount <= tickCount + 1;
+               if(tickCount-lastCount >= `TPS) begin
+                   secondsCount <= secondsCount + 1;
+                   if(pulseCount > 32) fastSecondsCount <= fastSecondsCount + 1;
+                   lastCount <= pulseCount;
                end
            end
        end
