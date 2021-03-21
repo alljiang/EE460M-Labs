ff4df35ea97fb71a161baed233d234c1e7ca1715
xchenyi <47702865+xchenyi@users.noreply.github.com>
2021-03-20 20:28:35 -0500

    nonblocked pulse

--- Lab 3/Lab3_VivadoProject/Lab3_VivadoProject.srcs/sources_1/new/PulseGenerator.v
+++ Lab 3/Lab3_VivadoProject/Lab3_VivadoProject.srcs/sources_1/new/PulseGenerator.v
@@ -34,56 +34,56 @@ module PulseGenerator(
         debug2 = !debug2; 
     
         if(!lastStart && start) begin
-            ticks = 0;
-            counter = 0;
-            outputPulse = 0;
-            lastSeconds = -1;
-            pulseCount = 0; 
+            ticks <= 0;
+            counter <= 0;
+            outputPulse <= 0;
+            lastSeconds <= -1;
+            pulseCount <= 0; 
         end
         lastStart = start;
     
        seconds = ticks / `TPS;
        if(mode == 2'b11) begin
-           if(seconds < 1) frequency = 20;
-           else if(seconds < 2) frequency = 33;
-           else if(seconds < 3) frequency = 66;
-           else if(seconds < 4) frequency = 27;
-           else if(seconds < 5) frequency = 70;
-           else if(seconds < 6) frequency = 30;
-           else if(seconds < 7) frequency = 19;
-           else if(seconds < 8) frequency = 30;
-           else if(seconds < 9) frequency = 33;
-           else if(seconds < 73) frequency = 69;
-           else if(seconds < 79) frequency = 34;
-           else if(seconds < 144) frequency = 124;
-           else frequency = 0;
+           if(seconds < 1) frequency <= 20;
+           else if(seconds < 2) frequency <= 33;
+           else if(seconds < 3) frequency <= 66;
+           else if(seconds < 4) frequency <= 27;
+           else if(seconds < 5) frequency <= 70;
+           else if(seconds < 6) frequency <= 30;
+           else if(seconds < 7) frequency <= 19;
+           else if(seconds < 8) frequency <= 30;
+           else if(seconds < 9) frequency <= 33;
+           else if(seconds < 73) frequency <= 69;
+           else if(seconds < 79) frequency <= 34;
+           else if(seconds < 144) frequency <= 124;
+           else frequency <= 0;
        end
        else begin
-           if(mode == 2'b00) frequency = 32;
-           else if(mode == 2'b01) frequency = 64;
-           else frequency = 128;
+           if(mode == 2'b00) frequency <= 32;
+           else if(mode == 2'b01) frequency <= 64;
+           else frequency <= 128;
        end
        
        if(!start) frequency = 0;
        
-        period = `TPS / ((frequency*2)+1);
+        period <= `TPS / ((frequency*2)+1);
        
        if(lastSeconds != seconds) begin
            //   first pulse of the second, let's force it to low
-           outputPulse = 0;
-           pulseCount = 0;
-           lastSeconds = seconds;
-           counter = 0;
+           outputPulse <= 0;
+           pulseCount <= 0;
+           lastSeconds <= seconds;
+           counter <= 0;
        end
        else if(counter >= period) begin
-           counter = 0;
-           outputPulse = !outputPulse;
+           counter <= 0;
+           outputPulse <= !outputPulse;
 //           if((pulseCount < frequency) || outputPulse) outputPulse = !outputPulse;
 //           if(outputPulse) pulseCount = pulseCount + 1;
        end
-       else counter = counter + 1;
+       else counter <= counter + 1;
        
-       ticks = ticks + 1;
+       ticks <= ticks + 1;
     end
     
     
