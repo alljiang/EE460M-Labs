5a2cc4ecb00f20bdd0bb4d6ab449a5ea102ad1af
Allen Jiang <alljiang@hotmail.com>
2021-03-31 19:32:58 -0500

    add starter code

---
+++ Lab 5/Lab 5.srcs/sources_1/imports/Downloads/clkdiv.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module clkdiv(
+    input clk,
+    output fsmclk 
+    );
+    
+    reg [14:0] count = 0;
+    reg out = 0;
+    assign fsmclk = out; 
+    
+    always @(posedge clk) begin 
+        count = count + 1;
+        if(count == 0) out = !out;
+    end
+
+endmodule
