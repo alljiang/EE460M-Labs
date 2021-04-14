eebc3d6e514b83504ccec09ecb2b17d6771045d4
xchenyi <47702865+xchenyi@users.noreply.github.com>
2021-04-14 15:38:00 -0500

    Lab6Start

---
+++ Lab6/Lab6.srcs/sources_1/new/MAC.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2021 03:36:17 PM
+// Design Name: 
+// Module Name: MAC
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MAC(
+
+    );
+endmodule
---
+++ Lab6/Lab6.srcs/sources_1/new/Matrix.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2021 03:36:17 PM
+// Design Name: 
+// Module Name: Matrix
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Matrix(
+
+    );
+endmodule
