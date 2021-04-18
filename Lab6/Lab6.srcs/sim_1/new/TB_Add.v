f8aa1e31c193c98e576cbc1677cc757067855c0c
xchenyi <47702865+xchenyi@users.noreply.github.com>
2021-04-18 12:16:53 -0500

    Add Testing prelim
    sanity check commit, add is written

--- Lab6/Lab6.srcs/sim_1/new/TB_Add.v
+++ Lab6/Lab6.srcs/sim_1/new/TB_Add.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -20,7 +21,50 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module TB_Add(
+module TB_Add();
+    reg St = 0;
+    reg rst = 0;
+    reg [7:0]A = 0;
+    reg [7:0]B = 0;
 
+    wire[7:0] Sum;
+
+
+Float_Add A1(
+    rst,
+    St,
+    A,
+    B,
+    Sum
     );
+
+initial begin  
+    St = 1;
+    A = 7'b00100000; //0.5
+    B = 7'b00100000; //0.5
+    if(Sum != 7'b00110000) 
+        $display("case incorrect");
+    #20
+    
+    St = 0;
+    A = 7'b00110000; //1
+    B = 7'b00010100; //0.3125
+    if(Sum != 7'b00010100) 
+        $display("case incorrect");
+    #20
+    
+    St = 1;
+    A = 7'b00000000;
+    B = 7'b00000000; //?
+    if(Sum != 7'b00000000) 
+        $display("case incorrect");
+    #20
+    
+    A = 7'b00000000;
+    B = 7'b00110101; //?
+    if(Sum != 7'b00110101) 
+        $display("case incorrect");
+
+end
+
 endmodule
