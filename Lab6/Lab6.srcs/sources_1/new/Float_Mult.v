9b8981056df0c7decc067fffa41ffc37b651a25f
xchenyi <47702865+xchenyi@users.noreply.github.com>
2021-04-18 11:43:28 -0500

    Revert "mult works now"
    This reverts commit 2572bf807f19a3c13f795c5f183116cfbf12b545.

--- Lab6/Lab6.srcs/sources_1/new/Float_Mult.v
+++ Lab6/Lab6.srcs/sources_1/new/Float_Mult.v
@@ -21,86 +21,107 @@
 
 
 module Float_Mult(
+    input CLK,
+    input St,
     input [7:0]A,
     input [7:0]B,
-    output reg[7:0] prod
+    output reg expOv,
+    output reg[7:0] prod,
+    output reg done
     );
 
 reg [9:0] prodBuf;
 reg [3:0] expBuf;
 reg ov;
-wire sign; 
-
+reg norm;
+wire sign;
 assign sign = A[7]^B[7];
 
-wire [4:0] AFrac;
-wire [2:0] AExp;
-wire [4:0] BFrac;
-wire [2:0] BExp;
-assign AFrac = {1'b1, A[3:0]};
-assign BFrac = {1'b1, B[3:0]};
-assign AExp = A[6:4] - 3; //exp offset = 3
-assign BExp = B[6:4] - 3;
+reg [4:0] AFrac;
+reg [2:0] AExp;
+reg [4:0] BFrac;
+reg [2:0] BExp;
 
-always @(*) begin
-    //0 case
-    if(A==0 || B==0) begin
-        expBuf = 0;
-        prodBuf = 0;
-    end
-    
-    else begin
-        prodBuf = AFrac * BFrac;
-        expBuf = AExp + BExp + 3;   //exponent with offset applied
-        
-        //check overflow 
-        if(prodBuf[9]==1)begin 
-            prodBuf = prodBuf >> 1;
-            expBuf = expBuf + 1;
-        end
-        
-        //check normalized
-        else if(prodBuf[9]==0 && prodBuf[8]==0) begin
-            //8 left shifts ensure floating point normalization
-            prodBuf = prodBuf << 1; 
-            expBuf = expBuf - 1;
-            
-            if(prodBuf[9]==0 && prodBuf[8]==0) begin
-                prodBuf = prodBuf << 1; 
-                expBuf = expBuf - 1;
-            end
-            if(prodBuf[9]==0 && prodBuf[8]==0) begin
-                prodBuf = prodBuf << 1; 
-                expBuf = expBuf - 1;
-            end
-            if(prodBuf[9]==0 && prodBuf[8]==0) begin
-                prodBuf = prodBuf << 1; 
-                expBuf = expBuf - 1;
-            end
-            if(prodBuf[9]==0 && prodBuf[8]==0) begin
-                prodBuf = prodBuf << 1; 
-                expBuf = expBuf - 1;
-            end
-            if(prodBuf[9]==0 && prodBuf[8]==0) begin
-                prodBuf = prodBuf << 1; 
-                expBuf = expBuf - 1;
-            end
-            if(prodBuf[9]==0 && prodBuf[8]==0) begin
-                prodBuf = prodBuf << 1; 
-                expBuf = expBuf - 1;
-            end
-            if(prodBuf[9]==0 && prodBuf[8]==0) begin
-                prodBuf = prodBuf << 1; 
-                expBuf = expBuf - 1;
-            end
-        end
-        
-    end
-    //load output reg
-        prod[7] = sign; 
-        prod[6:4] = expBuf[2:0]; 
-        prod[3:0] = prodBuf[7:4];
+reg [1:0] CS;
+reg [1:0] NS;
+
+initial begin
+    prodBuf = 0;
+    expBuf = 0;
+    CS = 0;
+    NS = 0;
 end
-      
+    
+always @(posedge CLK) begin
+    case(CS)
+        0: begin
+            done <= 0;
+            if(St) begin    //load all regs
+                NS <= 1;
+                AFrac <= {1'b1, A[3:0]};
+                BFrac <= {1'b1, B[3:0]};
+                AExp <= A[6:4] - 4; //exp offset = 4
+                BExp <= B[6:4] - 4;
+            end
+            else NS <= 0;
+        end
+        1: begin
+            done <= 0;
+            prodBuf = AFrac * BFrac;
+            expBuf = AExp + BExp + 4;
+            
+            if(prodBuf == 0) begin  //product is 0, special case
+                expBuf <= 4'b0000; //set exp as 0 for special case
+                NS <= 2;
+            end
+            else begin
+                if(prodBuf[9:6] > 0) begin //bit 5 is implied 1
+                    ov <= 1;    //fraction overflow, signal to shift right
+                    NS <= 1;    //stay in same state until fraction no overflow
+                end
+                else begin
+                    ov <= 0;
+                    NS <= 2;
+                end
+            end
+        end
+        2: begin    //normalization
+            if( !prodBuf[6] && prodBuf[5]) begin
+                norm <= 0;
+                NS <= 3;
+            end
+            else begin  //not normalized, left shift
+                norm <= 1;
+                NS <= 2;
+            end
+        end
+        3: begin
+            done <= 1;
+            if(expBuf+4 > 7) begin    //overflow detection in exponent
+                expOv <= 1;
+                NS <= 0;
+            end
+            else begin
+                expOv = 0;
+                NS <= 0;
+            end
+        end
+    endcase
+end
+    
+always @(posedge CLK) begin
+    CS <= NS;
+    if(ov) begin    //fraction overflow
+        prodBuf <= prodBuf >> 1;
+        expBuf <= expBuf + 1;
+    end
+    if(norm) begin  //normalize fraction
+        prodBuf <= prodBuf << 1;
+        expBuf <= expBuf - 1;
+    end
+    if(done) begin
+        prod = {sign, expBuf[2:0] + 4, prodBuf[4:0]};
+    end  
+end
 
 endmodule
