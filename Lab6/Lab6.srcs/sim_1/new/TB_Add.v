1be93b311bf53413ba17887218b3f376c7b4f1f5
Allen Jiang <alljiang@hotmail.com>
2021-04-18 19:57:45 -0500

    adder is working

--- Lab6/Lab6.srcs/sim_1/new/TB_Add.v
+++ Lab6/Lab6.srcs/sim_1/new/TB_Add.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -20,7 +21,59 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module TB_Add(
+module TB_Add();
+    reg St = 0;
+    reg rst = 0;
+    reg [7:0]A = 0;
+    reg [7:0]B = 0;
 
+    wire[7:0] Sum;
+
+
+Float_Add A1(
+    rst,
+    St,
+    A,
+    B,
+    Sum
     );
+
+initial begin  
+    St = 1;
+    
+    
+    A = 8'b00100000; //0.5
+    B = 8'b00100000; //0.5
+    #20
+    if(Sum != 8'b00110000) 
+        $display("case 1 incorrect");
+    
+    St = 1;
+    A = 8'b00010111;    //  0.359375
+    B = 8'b00011001;    //  0.39063
+    #20
+    if(Sum != 8'b00101000)  //  0.75
+        $display("case 2 incorrect");
+    
+    St = 1;
+    A = 8'b01100000;
+    B = 8'b01000100; 
+    #20
+    if(Sum != 8'b01100101)  //  10.5 
+        $display("case 3 incorrect");
+    
+    A = 8'b10011000;    //  -0.375
+    B = 8'b11000001;    //  -2.125
+    #20
+    if(Sum != 8'b11000100)  //  -2.5 
+        $display("case 4 incorrect");
+    
+    A = 8'b00110100;    //  1.25  
+    B = 8'b10111000;    //  -1.5  
+    #20
+    if(Sum != 8'b10010000)  //  -0.25 
+        $display("case 5 incorrect");
+
+end
+
 endmodule
--- Lab6/Lab6.srcs/sources_1/new/Float_Add.v
+++ Lab6/Lab6.srcs/sources_1/new/Float_Add.v
@@ -21,133 +21,131 @@
 
 
 module Float_Add(
-    input CLK,
+    input rst,
     input St,
     input [7:0] A,
     input [7:0] B,
-    output reg done,
-    output reg expOv,
     output reg[7:0] Sum
     );
 
-reg [5:0] sumBuf;
-reg [3:0] expBuf;
+reg [5:0] sumBuf; // for signed operation
+reg [10:0] fracBuf;
+reg [2:0] expBuf;
+reg signBuf;
 
-//control signals
-reg fracOv;
-reg expNoEq;
-reg LoadRegs;
-reg needNorm;
+initial begin 
+    sumBuf = 0;
+    fracBuf = 0;
+    expBuf = 0;
+    signBuf = 0;
+end
 
 //buffers for floating point components
-reg [4:0] AFrac;
+reg [5:0] AFrac;    //add 2 extra bits for sign and implied 1
 reg [2:0] AExp;
-reg [4:0] BFrac;
+reg [5:0] BFrac;
 reg [2:0] BExp;
 
-reg [1:0] CS;
-reg [1:0] NS;
-
-initial begin
-    sumBuf = 0;
-    expBuf = 0;
-    CS = 0;
-    NS = 0;
-end
-
-always @(posedge CLK) begin
-    case(CS) 
-        0: begin
-            done <= 0;
-            if(St) begin
-                LoadRegs <= 1;
-                NS <= 1;
-            end
-            else NS <= 0;
-        end
-        1: begin
-            done <= 0;
-            if(AFrac != BFrac) begin //keep shifting until exp equal
-                expNoEq = 1;
-                NS <= 1;
-            end
-            else begin
-                expNoEq = 0;
-                NS <= 2;
-            end
-        end
-        2: begin //now that exp equal add fracs
-            done <= 0;
-            sumBuf = AFrac + BFrac; 
-            if(sumBuf == 0) begin
-                sumBuf <= 0;
-                expBuf <= 0;
-                done <= 1;
-                NS <= 4;
-            end
-            else begin
-                if(sumBuf[5]) begin //fraction overflow case
-                    NS <= 3;
-                    fracOv <= 1;
-                end
-                else begin //no fraction overflow
-                    NS <= 3;
-                    fracOv <= 0;
-                end
-            end
-        end
-        3: begin    //normalize fractions
-            done <= 0;
-            if(sumBuf[5]==sumBuf[4]) begin //need normalization
-                needNorm <= 1;
-                NS <= 3;
-            end
-            else begin
-                needNorm <= 0;
-                NS <= 4;
-            end
-        end
-        4: begin    //exponent overflow check
-            if(expBuf + 4 > 7) begin //if exp > 3'b111, overflow 
-                expOv <= 1;
-                done <= 1;
-            end
-            else begin
-                expOv <= 0;
-                done <= 1;
-            end
-        end
-    endcase
-end
-
-always @(posedge CLK) begin
-    CS <= NS;
-    if (LoadRegs) begin
-        AFrac <= {1'b1, A[3:0]};
-        BFrac <= {1'b1, B[3:0]};
-        AExp <= A[6:4] - 4; //exp offset = 4
-        BExp <= B[6:4] - 4;
+always @(*) begin
+    if(rst) begin
+        Sum = 8'b00000000;
     end
-    if (expNoEq) begin
-        if(BExp < AExp) begin
-            BExp <= BExp + 1;
-            BFrac <= BFrac >> 1;
+    else if(St) begin
+        AFrac = {1'b0,1'b1, A[3:0]};
+        BFrac = {1'b0, 1'b1, B[3:0]};
+        AExp = A[6:4];
+        BExp = B[6:4];
+        
+        //A bigger exponent
+        if(AExp > BExp)begin
+            expBuf = AExp; 
+            //shift smaller number, use larger exponent value
+            BFrac = BFrac >> (AExp-BExp);
         end
+        
+        //B bigger exponent
+        else if (AExp < BExp) begin
+            expBuf = BExp; 
+            //shift smaller number, use larger exponent value
+            AFrac = AFrac >> (BExp-AExp);
+        end
+        
+        //Exponents Equal
         else begin
-            AExp <= AExp + 1;
-            AFrac <= AFrac >> 1;
+            expBuf = AExp;
         end
-    end
-    if (fracOv) begin
-       sumBuf <= sumBuf >> 1;
-       expBuf <= expBuf + 1;
-    end  
-    if (needNorm) begin
-        sumBuf <= sumBuf << 1;
-        expBuf <= expBuf - 1;
-    end
-    if(done) begin
-        Sum <= {sumBuf[5], expBuf[2:0], sumBuf[3:0]};
+        
+        if(A[7] == B[7]) signBuf = A[7];
+        else begin
+            if(AFrac > BFrac) begin
+                signBuf = A[7];
+                BFrac = ~BFrac + 1;
+            end                
+            else if(AFrac < BFrac) begin
+                signBuf = B[7];
+                AFrac = ~AFrac + 1;
+            end
+            else signBuf = 0;
+        end
+        
+        sumBuf = AFrac + BFrac;
+        
+        //normalize fractions
+        fracBuf = sumBuf[5:0]; //chop off extra sign bits
+        if(fracBuf >= 6'b100000) begin
+            fracBuf = fracBuf >> 1;
+            expBuf = expBuf + 1;
+        end
+        if(fracBuf >= 6'b100000) begin
+            fracBuf = fracBuf >> 1;
+            expBuf = expBuf + 1;
+        end
+        if(fracBuf >= 6'b100000) begin
+            fracBuf = fracBuf >> 1;
+            expBuf = expBuf + 1;
+        end
+        if(fracBuf >= 6'b100000) begin
+            fracBuf = fracBuf >> 1;
+            expBuf = expBuf + 1;
+        end
+        if(fracBuf >= 6'b100000) begin
+            fracBuf = fracBuf >> 1;
+            expBuf = expBuf + 1;
+        end
+        if(fracBuf >= 6'b100000) begin
+            fracBuf = fracBuf >> 1;
+            expBuf = expBuf + 1;
+        end
+        if(fracBuf >= 6'b100000) begin
+            fracBuf = fracBuf >> 1;
+            expBuf = expBuf + 1;
+        end
+        if(fracBuf < 6'b10000) begin
+            fracBuf = fracBuf << 1;
+            expBuf = expBuf - 1;
+        end
+        if(fracBuf < 6'b10000) begin
+            fracBuf = fracBuf << 1;
+            expBuf = expBuf - 1;
+        end
+        if(fracBuf < 6'b10000) begin
+            fracBuf = fracBuf << 1;
+            expBuf = expBuf - 1;
+        end
+        if(fracBuf < 6'b10000) begin
+            fracBuf = fracBuf << 1;
+            expBuf = expBuf - 1;
+        end
+        if(fracBuf < 6'b10000) begin
+            fracBuf = fracBuf << 1;
+            expBuf = expBuf - 1;
+        end
+        
+        
+        
+        Sum = {signBuf, expBuf, fracBuf[3:0]};
+                
+        if(A == 0 && B==0) Sum = 8'b00000000;
     end
 end
 
