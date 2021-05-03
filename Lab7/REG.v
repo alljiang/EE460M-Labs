6cfd475f218afdbaa69ccc919286c8d390faf27a
xchenyi <47702865+xchenyi@users.noreply.github.com>
2021-05-02 23:10:51 -0500

    no more red :)

--- Lab7/REG.v
+++ Lab7/REG.v
@@ -13,6 +13,7 @@ module REG(CLK, RegW, DR, SR1, SR2, Reg_In, ReadReg1, ReadReg2, OUT, R1_CTRL, Te
   input [31:0] Reg_In;
   output reg [31:0] ReadReg1;
   output reg [31:0] ReadReg2;
+
   output [31:0] R2OUT;
   output [7:0] OUT;
   input [2:0] R1_CTRL;
@@ -20,6 +21,23 @@ module REG(CLK, RegW, DR, SR1, SR2, Reg_In, ReadReg1, ReadReg2, OUT, R1_CTRL, Te
 
   reg [31:0] REG [0:31];
   integer i;
+
+wire [31:0]R3;
+wire [31:0]R4;
+wire [31:0]R5;
+wire [31:0]R6;
+wire [31:0]R7;
+wire [31:0]R8;
+
+assign R8 = REG[8];
+assign R3 = REG[3];
+assign R4 = REG[4];
+assign R5 = REG[5];
+assign R6 = REG[6];
+assign R7 = REG[7];
+
+
+
 
   initial begin
     ReadReg1 = 0;
